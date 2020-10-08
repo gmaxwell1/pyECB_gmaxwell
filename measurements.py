245a99844e2ffb70d260994310fb065895015946
gmaxwell1 <71451675+gmaxwell1@users.noreply.github.com>
2020-10-08 12:14:12 +0200

    Update measurements.py

--- measurements.py
+++ measurements.py
@@ -10,7 +10,7 @@ Author: Nicholas Meinhardt, Maxwell Guerne-Kieferndorf (QZabre)
 
 Date: 07.10.2020
 """
-
+#HIHIHIHIHIHIHI
 ########## Standard library imports ##########
 import numpy as np
 import serial
