67d29792b769a14f69dd0151016645d233d1b128
Nicholas Meinhardt <nmeinhar@ad.phys.ethz.ch>
2020-12-16 15:19:26 +0100

    added new script and functions for trilinear and triquadratic fitting

--- modules/data_analysis_accuracy_characterization.py
+++ modules/data_analysis_accuracy_characterization.py
@@ -16,7 +16,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-########## local imports ##########
+# local imports 
 try:
     from modules.analysis_tools import *
 except ModuleNotFoundError:
@@ -27,66 +27,12 @@ finally:
     from modules.interpolation_tools import find_start_of_saturation
 
 
-#%%
-def collectAndExtract(directory, B_min, remove_saturation = True,
-                        verbose=False, fraction_cutoff = 0.02,
-                        affine_fct = lambda x, a, b: a*x + b):
-
-    # collect all csv-files in this directory
-    filenames = []
-    [filenames.append(file) for file in os.listdir(directory) if file.endswith(".csv")]
-    
-    # if in list, remove the linear_fits file from previous fits
-    try:
-        filenames.remove('linear_fits.csv')
-    except ValueError:
-        pass
-    print(len(filenames))
-
-    # loop through all csv files in a dictionary and fit the data
-    for i in range(len(filenames)):
-        if verbose:
-            print(filenames[i])
-
-        # read in raw measurment data
-        data_filepath = os.path.join(directory, filenames[i])
-        I, mean_data, std_data, expected_fields = extract_raw_data_from_file(data_filepath)
-
-        # -> this could be used to exclude data above saturation, but could be left out
-        # estimate minimum and maximum indices of region within which the linear relation between current and field holds  
-        # even though find_start_of_saturation offers the possibility to specify the considered component, 
-        # keep the default stting, which detects the field component that has the greatest absolute field values. 
-        # This should work fine for situations, where one component is dominating. 
-        i_min, i_max = find_start_of_saturation(I, mean_data, std_data, fraction_cutoff=fraction_cutoff,
-                                fitting_fct = affine_fct)
-
-        # estimate field magnitudes
-        magnitudes = np.linalg.norm(mean_data, axis=1)
-
-        # set up mask to only keep data with magnitudes larger than B_min
-        mask_keep = magnitudes >= B_min
-
-        # optionally: also remove potentially saturated part
-        if remove_saturation:
-            mask_keep[:i_min+1] = False
-            mask_keep[i_max:] = False
-
-        # collect all relevant data
-        if i == 0:
-            B_measured = mean_data[mask_keep]
-            B_expected = expected_fields[mask_keep]
-        else:
-            B_measured = np.append(B_measured, mean_data[mask_keep], axis=0)
-            B_expected = np.append(B_expected, expected_fields[mask_keep], axis=0)
-
-    print(B_measured.shape)
-    return B_measured, B_expected
 
 
 #%%
 directory = '../test_data/config_tests_20_11_10'
 threshold = 10
-B_measured, B_expected = collectAndExtract(directory, threshold, remove_saturation=True)
+currents, B_measured, B_expected = collectAndExtract(directory, threshold, remove_saturation=True)
 
 
 #%%
